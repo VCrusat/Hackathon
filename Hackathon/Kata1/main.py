79b9e46f07e7dd0f4a6b78c8ce819fca7fef3bac
Victor Crusat <vicrude@campusaula.com>
2020-05-16 12:18:27 +0200

    K1_v1.0

---
+++ Hackathon/Kata1/main.py
@@ -0,0 +1,6 @@
+precio = 3.49
+descuento = 0.4
+precio_descuento = precio * descuento
+numero_barras = int(input('Introduce el número de barras: '))
+coste_final = numero_barras * precio_descuento
+print('El coste final es de: '+str(coste_final))
